e639ef6215cd913c013d45ac653e0b82b1985605
minxomat <minxomat@gmail.com>
2017-02-18 21:41:48 +0100

    Create phucket.php

---
+++ phucket.php
@@ -0,0 +1,30 @@
+<?php
+
+function phucket($iSecDelay) {
+  $sIPHash    = md5($_SERVER[REMOTE_ADDR]);
+  $bReqAllow  = -1;
+  $sContent   = "";
+
+  if (!($nFileHandle = fopen("bucket.cache", "c+"))) return -1;
+
+  flock($nFileHandle, LOCK_EX);
+
+  while ($sCurLine = fgets($nFileHandle, 4096)) {
+    if ( (time() - strtok($sCurLine, '|')) <= $iSecDelay ) {
+      $sContent .= $sCurLine.PHP_EOL;
+      continue;
+    }
+
+    if (strpos($sCurLine, $sIPHash) !== false) {
+      $sContent .= time()."|".$sIPHash.PHP_EOL;
+      $bReqAllow = 1;
+    }
+  }
+
+  ftruncate($nFileHandle, 0);
+  rewind($nFileHandle);
+  fwrite($nFileHandle, $sContent);
+  flock($nFileHandle, LOCK_UN);
+  fclose($nFileHandle);
+
+  return $bReqAllow;
